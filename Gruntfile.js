0cf2517a377686822b30355a30806a27fecca7ac
alberlau@gmail.com <alberlau@gmail.com>
2015-06-09 19:05:21 +0300

    Refactoring

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,19 +5,33 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON('package.json'),
     uglify: {
       options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+         banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+				,compress : true
       },
-      build: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
+      my_target: {
+				 src:['dist/<%= pkg.name %>.js']
+				,dest : 'dist/<%= pkg.name %>.min.js'
       }
     }
+		,concat : {
+			options: {
+				separator : ';'
+			}
+			,dist: {
+				src : [
+					 'src/js/core/Clazz.js'
+					,'src/js/core/spahql.js'
+					,'src/js/forms/BaseForm.js'
+				]
+				,dest : 'dist/<%= pkg.name %>.js'
+			}
+		}
+		,clean : ['dist/<%= pkg.name %>.js','dist/<%= pkg.name %>.min.js']
   });
 
-  // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-clean');
 
-  // Default task(s).
-  grunt.registerTask('default', ['uglify']);
-
+	grunt.registerTask('build', ['concat','uglify']);
 };
