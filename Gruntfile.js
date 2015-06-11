64190795816362b9bafa4a4f9793ac13e541f2c9
alberlau <alberlau@gmail.com>
2015-06-11 09:19:08 +0300

    GRUNT

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,6 +17,19 @@ module.exports = function(grunt) {
 			options: {
 				separator : ';'
 			}
+      ,basic_and_extras : {
+        files : {
+          'dist/<%= pkg.name %>.js' : [
+		  	  	 'src/js/core/Clazz.js'
+	  				,'src/js/core/spahql.js'
+  					,'src/js/forms/BaseForm.js'
+				  ]
+          ,'dist/<%= pkg.name %>IE.js' : [
+		  			 'src/assets/js/html5shiv.js'
+	  				,'src/assets/js/respond.min.js'
+          ] 
+        }
+      }
 			,dist: {
 				src : [
 					 'src/js/core/Clazz.js'
@@ -26,12 +39,32 @@ module.exports = function(grunt) {
 				,dest : 'dist/<%= pkg.name %>.js'
 			}
 		}
-		,clean : ['dist/<%= pkg.name %>.js','dist/<%= pkg.name %>.min.js']
+    ,cssmin : {
+      target : {
+        files :{
+            'dist/<%= pkg.name %>.min.css' : 
+              [
+                 'src/assets/plugins/bootstrap/css/bootstrap.css'
+                ,'src/assets/css/main.css'
+                ,'src/assets/css/theme.css'
+                ,'src/assets/css/MoneAdmin.css'
+                ,'src/assets/plugins/Font-Awesome/css/font-awesome.css'
+                ,'src/assets/css/layout2.css'
+                ,'src/assets/plugins/flot/examples/examples.css'
+                ,'src/assets/plugins/timeline/timeline.css'
+                ,'src/assets/plugins/dataTables/dataTables.bootstrap.css'
+              ]
+          }
+      
+      }
+    }
+		,clean : ['dist/<%= pkg.name %>.js','dist/<%= pkg.name %>.min.js','dist/<%= pkg.name %>.min.css']
   });
 
   grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
 
-	grunt.registerTask('build', ['concat','uglify']);
+	grunt.registerTask('build', ['concat','uglify','cssmin']);
 };
