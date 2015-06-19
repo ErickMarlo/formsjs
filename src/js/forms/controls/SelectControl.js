2f857fb7c9f02ca17a0310b6c5f4919162e871ac
alberlau <alberlau@gmail.com>
2015-06-19 13:45:59 +0300

    Select control

--- src/js/forms/controls/SelectControl.js
+++ src/js/forms/controls/SelectControl.js
@@ -5,7 +5,27 @@ forms.controls.SelectControl=forms.controls.BaseControl.extend({
 		var $fld=forms.controls.ControlManagerInstance.renderer.renderSelectField(fld);
 		var select=$($fld).find('select');
 		this._super(fld,select);
-		
+		var ctx=this;
+		fld.load=function(lopt,$jq){
+			ctx.load.call(fld,lopt,$jq);
+		};
+		fld.load(fld.options,select);
 		return $fld;
+	}
+	,load: function(lopt,$jq){
+		if(!$jq) $jq=this.$jq;
+		for(var i=0;i<lopt.length;i++) {
+			var opt=lopt[i];
+			if(opt.options) {
+				var $optgrp=$('<optgroup label="'+opt.text+'"></optgroup>');
+				$optgrp.data('data',opt)
+				$jq.append($optgrp);
+				this.load(opt.options,$optgrp);
+			} else {
+				var $opt=$('<option value="'+opt.value+'">'+opt.text+'</option>');
+				$opt.data('data',opt);
+				$jq.append($opt);
+			}
+		}
 	}
 });
