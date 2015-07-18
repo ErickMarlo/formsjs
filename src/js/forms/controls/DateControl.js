5c4ad6a0b3882b79158054e9bc7541f5f1b76124
alberlau@gmail.com <alberlau@gmail.com>
2015-07-18 07:19:30 +0300

    DateControl change event support

--- src/js/forms/controls/DateControl.js
+++ src/js/forms/controls/DateControl.js
@@ -1,14 +1,16 @@
 Package.Register('forms.controls');
 
 forms.controls.DateControl=forms.controls.TextControl.extend({
-	renderField : function(fld) {
-		var $inp=$($fld).find('input');
-		var $fld=this._super(fld,$inp);
-		return $fld;
-	}
-	,onafterrender : function(fld){
+	onafterrender : function(fld){
 		this._super(fld);
 		var $inp=fld.$jq;
 		$inp.datepicker({format:'dd/mm/yyyy'});
+	}
+	,setupvaluechange: function(fld){
+		this._super(fld);
+		fld.$jq.on('changeDate',function(ev){
+			if(fld.change) fld.change(ev);
+			fld.form.change(fld,ev);
+		});
 	}
 });
